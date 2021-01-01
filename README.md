ddbd1422f0c7ba8037a63b259c097a920d462cda
Ange Rodriguez <ange.rodriguez@thunderwave.fr>
2021-01-01 02:16:18 +0100

    style (victim) : Root page

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 # CookieStealer
 
-![Learn goal](https://img.shields.io/badge/Learn-XSS%20(Cross%20Site%20Scripting)-blueviolet) ![Minimum PHP Version](https://img.shields.io/badge/PHP--version-%3E%3D7.4-informational)
+![Learn goal](https://img.shields.io/badge/Learn-XSS%20(Cross%20Site%20Scripting)-blueviolet) ![Minimum PHP Version](https://img.shields.io/badge/PHP--version-%3E%3D7.4-informational) ![Status](https://img.shields.io/badge/Status-Work%20in%20progress-yellow)
 
 </div>
--- victim/index.php
+++ victim/index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Victim server</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Victim server</h1>
+    <main class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-6">
+                <div class="card text-center mt-5">
+                    <div class="card-header">Hey!</div>
+                    <div class="card-body">
+                        <h5 class="card-title">I'm a honest website.</h5>
+                        <p class="card-text text-danger mt-3">But I've a XSS fault and all hackers can exploit it. In this case, a cookie stealer script are injected inside the DOM.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
